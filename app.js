44c5da7fcf19421efd09bc9e79e09d24f0ecb462
Noor Ali <nooralib180@gmail.com>
2024-01-30 23:31:59 +0500

    implementing a route to get a single tour

--- app.js
+++ app.js
@@ -41,6 +41,26 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+// getting a single tour by id, using req.params...
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: "fail",
+      message: "INVALID_ID",
+    });
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour,
+    },
+    error: "error",
+  });
+});
+
 // route to handle post request for tours data...
 app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
