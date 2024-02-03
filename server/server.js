f6d78052d3a6e37a831d79100b4d4d5decbfd01d
Noor Ali <nooralib180@gmail.com>
2024-02-03 19:17:31 +0500

    connecting to mongoDB database through mongoose

--- server/server.js
+++ server/server.js
@@ -5,21 +5,21 @@ const app = require("../app");
 
 console.log(process.env.NODE_ENV);
 
-// const DB = process.env.DATABASE.replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   })
-//   .then((con) => {
-//     console.log(con.connections);
-//     console.log("DB connection successful");
-//   });
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log("DB connection successful");
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
