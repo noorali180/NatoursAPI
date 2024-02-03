13686fb426def9e484bc084118606a56440ef873
Noor Ali <nooralib180@gmail.com>
2024-02-03 19:51:12 +0500

    creating a simple model for tours and specifying the schema

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const app = require("../app");
 
@@ -20,6 +21,27 @@ mongoose
     console.log(con.connections);
     console.log("DB connection successful");
   });
+
+// to do CRUD operations we need to create a mongoose model, and for model creation we need a schema...
+const tourSchema = new Schema({
+  name: {
+    // we can simply type the name of type of field, but with specifying type with object we can add some extra things in it, like required, unique, default and many more....
+    type: String,
+    // requirted: [true/false, "error_message"] --> bascially it is a validator...
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = model("Tour", tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
