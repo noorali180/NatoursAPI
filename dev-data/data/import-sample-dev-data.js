e51e662a31091abd233badb6654822306058f806
Noor Ali <nooralib180@gmail.com>
2024-02-05 07:57:22 +0500

    creating a small script for importing and deleting the sample data to database

---
+++ dev-data/data/import-sample-dev-data.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const dotenv = require("dotenv").config({
+  path: "../../../natours-with_API/.env",
+});
+
+const mongoose = require("mongoose");
+
+const Tour = require("./../../models/tourModel");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("DB connection successful");
+  });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`));
+
+async function importData() {
+  try {
+    await Tour.create(tours);
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+}
+
+async function deleteData() {
+  try {
+    await Tour.deleteMany();
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+}
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
+
+console.log("running...");
