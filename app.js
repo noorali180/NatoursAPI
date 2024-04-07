3a565c06749d39d9972cd7e97ee321eecc67dbfd
Noor Ali <nooralib180@gmail.com>
2024-04-07 21:17:46 +0500

    handling the unhandled routes in express

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 // Handling the unhandled routes, e.g. routes which are not defined in server...(it will work because middlewares runs as order in the codebase)
+// if the code reaches this stage which means the request response cycle is not yet completed, which also means that the requested url is not matched with any of the route we have defined...
 app.all("*", (req, res, next) => {
   res.status(404).json({
     status: "fail",
