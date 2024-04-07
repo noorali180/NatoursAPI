ef3a593cc4f68b8a05690d2d6eb8cad341feab32
Noor Ali <nooralib180@gmail.com>
2024-04-07 21:14:58 +0500

    handling the unhandled routes in express

--- app.js
+++ app.js
@@ -30,4 +30,12 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// Handling the unhandled routes, e.g. routes which are not defined in server...(it will work because middlewares runs as order in the codebase)
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
