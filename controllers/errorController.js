4b173544af418acf8d9f44703baa45d0d3fb5827
Noor Ali <nooralib180@gmail.com>
2024-04-15 22:22:13 +0500

    hadnling invalid database ids

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 // send error in development...
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -47,6 +54,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    // 1) casting error (invalid id)
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
