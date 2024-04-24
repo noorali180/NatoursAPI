2748d4e13c0c0ea9e2a6bb0e8343cf719c73dde8
Noor Ali <nooralib180@gmail.com>
2024-04-24 21:15:08 +0500

    handling the uncaught rejections (wrong DB password), rejected promises

--- server/server.js
+++ server/server.js
@@ -25,3 +25,17 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app is listening on port ${port}...`);
 });
+
+////////////////////////////////////////////////////////////
+
+// Handling unhandled rejections which are outside the express app therefore our global error handling middleware will not catch it, such as wrong database password... (with the help of events and event listeners of node JS).
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+
+  // it is better to use a callback function with server close function, which will wait until server finish all fetching (requests) and then exit the program, rather than exiting the program directly,
+  server.close(() => {
+    process.exit(1); // 1 means unhandled exception, 0 means success...
+  });
+});
