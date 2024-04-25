44ce8663494b9d59a37f0e3dff28c9cd4484bf1c
Noor Ali <nooralib180@gmail.com>
2024-04-25 14:06:14 +0500

    managing passwords (encrypting or hashing)

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Please tell us your name!"] },
   email: {
@@ -28,6 +30,28 @@ const userSchema = new mongoose.Schema({
     },
   },
 });
+
+// Encryption and Decryption of passwords (using )
+// in document middleware, time before saving any document.
+userSchema.pre("save", async function (next) {
+  // if password is not modified then return from the function...
+  if (!this.isModified("password")) next();
+
+  // else run this code...
+
+  // using bcrypt hashing algorithm for encryption (to avoid brute force attacks from hackers)...
+  // this algorithm will first solve and then hash our password to make it really strong. (also adding a random string at the end to avoid same hash for two different decryptions)
+
+  // encrypting (hashing) the password using bcryptjs library, with 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // deleting passwordConfirm field from DB... (because we need it only for validation and not for encryption)
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+/////////////////////////////////////////////////////
 
 const User = mongoose.model("User", userSchema);
 
