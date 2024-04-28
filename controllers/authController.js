1fe4f1bee22f5c2f667bae07bc95d174934d736f
Noor Ali <nooralib180@gmail.com>
2024-04-28 21:27:20 +0500

    authntication of users with jwt(jsonwebtoken)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,25 @@
+const jwt = require("jsonwebtoken");
+
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  // secret key, should be at least of 32 characters long string for more powerful and secure security...
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
