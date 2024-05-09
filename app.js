f4d98c11b8b9a8a7e7746307e39feaca9a17837a
Noor Ali <nooralib180@gmail.com>
2024-05-09 16:41:34 +0500

    data sanitization

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 // one separate router for each resource...
 const tourRouter = require("./routes/tourRoutes");
@@ -44,7 +46,13 @@ app.use(cookieParser());
 // body parser, reading data from body into req.body...
 app.use(express.json({ limit: "10kb" }));
 
-// Serving static files...
+// Data sanitization against NoSQL query injection...
+app.use(mongoSanitize());
+
+// Data sanitization against XSS (cross site scripting attack)...
+app.use(xss());
+
+// serving static files...
 app.use(express.static(`${__dirname}/public`));
 
 // creating a custom middleware...
