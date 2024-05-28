af9d2ad6dbdba3273ae50b746044f21f8891de09
Noor Ali <nooralib180@gmail.com>
2024-05-28 09:17:53 +0500

    refactoring security within review routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,13 +5,28 @@ const authController = require("../controllers/authController");
 
 const router = express.Router({ mergeParams: true });
 
+// routes defined after this middleware will be protected...
+router.use(authController.protect);
+
 router
   .route("/")
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo("user"),
+    reviewController.setTourUserIds,
     reviewController.createReview
+  );
+
+router
+  .route("/:id")
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo("admin", "user"),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo("admin", "user"),
+    reviewController.deleteReview
   );
 
 module.exports = router;
