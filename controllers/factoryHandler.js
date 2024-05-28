d96ab52a5ef7655e062bd2d256e339617e14ab29
Noor Ali <nooralib180@gmail.com>
2024-05-28 08:39:43 +0500

    adding a factory handler to handle all the repeated tasks at one place

---
+++ controllers/factoryHandler.js
@@ -0,0 +1,52 @@
+const AppError = require("./../utils/appError");
+const catchAsync = require("./../utils/catchAsync");
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError("No tour found with that ID.", 404));
+    }
+
+    res.status(204).json({
+      status: "success",
+      requestedTime: req.requestedTime,
+      data: null,
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError("No tour found with that ID.", 404));
+    }
+
+    res.status(201).json({
+      status: "success",
+      requestedTime: req.requestedTime,
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      requestedTime: req.requestedTime,
+      data: {
+        data: doc,
+      },
+    });
+
+    next();
+  });
---
+++ controllers/tourController-v3.js
@@ -0,0 +1,187 @@
+const fs = require("fs");
+
+const Tour = require("../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+// function to get all the tours...
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  // console.log(req.query);
+  //// For making & bettering the query...
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .fieldsLimiting()
+    .pagination();
+
+  //// CONSUMING QUERY WITH AWAIT...
+  const tours = await features.query;
+
+  //// RESPONSE...
+  res.status(200).json({
+    status: "success",
+    requestedTime: req.requestedTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+// function to get a single tour...
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id).populate("reviews");
+  // Tour.findOne({_id: req.params.id});
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID.", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    requestedTime: req.requestedTime,
+    data: {
+      tour,
+    },
+  });
+});
+
+// function to create a new tour...
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    requestedTime: req.requestedTime,
+    data: {
+      tour: newTour,
+    },
+  });
+});
+
+// function to update an existing tour...
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // return the new updated tour...
+    runValidators: true, // validators should run again, compare with schema...
+  });
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID.", 404));
+  }
+
+  res.status(201).json({
+    status: "success",
+    requestedTime: req.requestedTime,
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+// function to delete a tour...
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID.", 404));
+  }
+
+  res.status(204).json({
+    status: "success",
+    requestedTime: req.requestedTime,
+    data: null,
+  });
+});
+
+/////////////////////////////// AGGREGATION PIPELINE ////////////////////////////////
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    { $match: { ratingsAverage: { $gte: 4.5 } } },
+    {
+      $group: {
+        // it is groupBY identifier which will group the data based on that field...
+        // _id: "null",
+        // _id: "$difficulty",
+
+        _id: { $toUpper: "$difficulty" },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: "$ratingsQuantity" },
+        avgRating: {
+          $avg /*mathematical field to calc average*/:
+            "$ratingsAverage" /*field name in db*/,
+        },
+        avgPrice: {
+          $avg: "$price",
+        },
+        minPrice: { $min: "$price" },
+        maxPrice: { $max: "$price" },
+      },
+    },
+    {
+      $sort: {
+        avgPrice: -1,
+      },
+    },
+    // {
+    //   $match: {
+    //     _id: { $ne: "EASY" },
+    //   },
+    // },
+  ]);
+
+  res.status(200).json({
+    status: "success",
+    requestedTime: req.requestedTime,
+    data: {
+      stats,
+    },
+  });
+});
+
+// To calculate the busiest month of the given year...
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = +req.params.year;
+
+  const plan = await Tour.aggregate([
+    {
+      $unwind: "$startDates",
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $month: "$startDates" },
+        numOfTours: { $sum: 1 },
+        tours: { $push: "$name" },
+      },
+    },
+    {
+      $addFields: { month: "$_id" },
+    },
+    {
+      $project: { _id: 0 },
+    },
+    {
+      $sort: { numOfTours: -1 },
+    },
+    // {
+    //   $limit: 2,
+    // },
+  ]);
+
+  res.status(200).json({
+    status: "success",
+    requestedTime: req.requestedTime,
+    data: {
+      plan,
+    },
+  });
+});
