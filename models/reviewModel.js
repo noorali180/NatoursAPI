2fc19a1bbec38a680a690eb539dff5fc7165fbee
Noor Ali <nooralib180@gmail.com>
2024-05-29 15:31:28 +0500

    calculating ratingsAverage and saving it to tour document by using static function and post save document middleware on Review Model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -58,27 +58,22 @@ reviewSchema.statics.calcAvgRatings = async function (tourId) {
 
   // now we need to update the ratings quantity and no. of ratings in the Tour document...
   // ratingsQuantity, ratingsAverage
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRatings,
-    ratingsAverage: stats[0].avgRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRatings,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
 // NOTE: we have to run this function whenever a new rating is saved or an existing review is deleted, we can do it by using document middleware...
 
 ///////////////////// MIDDLEWARES //////////////////////////////
-
-// 1) DOCUMENT MIDDLEWARE
-reviewSchema.post("save", function () {
-  // the static function can be called upon the Model (Review Model)...
-  // Review.calcAvgRatings(this.tour); "this" will points to the current doc.
-
-  // this.constructor => this points the Model.
-
-  this.constructor.calcAvgRatings(this.tour);
-});
-
-// 2) QUERY MIDDLEWARE
 
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
@@ -96,6 +91,39 @@ reviewSchema.pre(/^find/, function (next) {
 
   next();
 });
+
+// Middlewares which will handle calculating and updating ratingsAverage whenever a review is created, deleted or updated...
+
+// when a review is created or save (document middleware)
+reviewSchema.post("save", function () {
+  // the static function can be called upon the Model (Review Model)...
+  // Review.calcAvgRatings(this.tour); "this" will points to the current doc.
+
+  // this.constructor => this points the Model.
+
+  this.constructor.calcAvgRatings(this.tour);
+});
+
+// when a review is updated or deleted (query middleware)
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // we need to find a way to access the current document in order to perform operations, but in query middleware we have only access to the current query...
+
+  // const doc = await this.model.findOne(); // => will return the current document.
+  this.doc = await this.model.findOne(this.getQuery());
+
+  // so in this way we can get access to updated document in post middleware...
+  // (passing the data from a pre middleware to the post middleware)
+
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // this.model.findOne(), this does not work here, query is already executed...
+  await this.doc.constructor.calcAvgRatings(this.doc.tour);
+});
+
 const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
